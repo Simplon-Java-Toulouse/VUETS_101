e9acc176389c9fc4597e2e3e1b38e3c4b1328c00
timothé <timothe.laude@gmail.com>
2019-03-29 12:28:24 +0100

    exo fait

---
+++ src/components/Conditon.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+	
+ 	<p v-if="bool">{{visible}}</p>
+ 	<p v-if="!bool">{{cacher}}</p>
+ 	<button @click="toggle(bool)">Toggle</button>
+ </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue,Prop } from 'vue-property-decorator';
+
+
+@Component	
+export default class Condition extends Vue {
+	bool: boolean=false;
+	cacher: string=""
+	visible: string="je suis cacher"
+	@Prop(Boolean) bool:boolean;
+	toggle(bool): void{
+		if(this.bool)
+			this.bool=false
+		else
+			this.bool=true
+	}
+	
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Declaratif.vue
@@ -0,0 +1,31 @@
+<template>
+	<section>
+		<input type="text" :value="name" @input="changeNom">
+  		Bonjour {{nom}}
+  	</section>
+</section>
+
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default  class Declaratif extends Vue{
+	nom:string="TOTO"
+	@Prop(String) name:string
+	
+		changeNom(event): void{
+			this.nom=event.target.value
+			this.$emit('nomChanged',this.nom);
+		
+	}
+}
+
+	
+
+</script>
+
+<style>
+
+</style>
